7d3a88da91b97e0d152f63b15456422dfc2fdc3b
Marek Majkut <marek.majkut@takeda.com>
2022-12-08 13:18:49 +0100

    eslint to devops added

--- devops/cicd/components/s3-buckets.ts
+++ devops/cicd/components/s3-buckets.ts
@@ -1,10 +1,3 @@
-// import {
-//   aws_s3 as s3,
-//   Stack,
-//   RemovalPolicy
-// } from 'aws-cdk-lib'
-// import { getResourceId } from '../utils/param-utils'
-
 import { TerraformStack } from 'cdktf'
 import { S3Bucket } from '@cdktf/provider-aws/lib/s3-bucket'
 import { getResourceId } from '../utils/param-utils'
--- devops/cicd/main.ts
+++ devops/cicd/main.ts
@@ -1,12 +1,11 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
-import { App, CloudBackend, NamedCloudWorkspace } from "cdktf";
+import { App, CloudBackend, NamedCloudWorkspace } from 'cdktf'
 import { getBranchName, getStackId } from './utils/param-utils'
 import { STACK_ID_DEFAULT } from './constants/proj-info'
 import { MyStack } from './components/tf-stacks'
 
-
-const app = new App();
+const app = new App()
 
 const stackId = getStackId() ?? STACK_ID_DEFAULT
 const branchName = getBranchName()
@@ -20,10 +19,10 @@ console.log(' Please review the stack name: ', stackId)
 // console.log(' Region: ', region, ' Profile: ', profile)
 console.log('-----------------------------------------------------')
 
-const stack = new MyStack(app, stackId);
+const stack = new MyStack(app, stackId)
 new CloudBackend(stack, {
-  hostname: "app.terraform.io",
-  organization: "marekmajkut",
-  workspaces: new NamedCloudWorkspace("proj-tf")
-});
-app.synth();
+  hostname: 'app.terraform.io',
+  organization: 'marekmajkut',
+  workspaces: new NamedCloudWorkspace('proj-tf')
+})
+app.synth()
