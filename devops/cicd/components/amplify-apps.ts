55bab2cb87b1756451212b31e20a8e4103ad3636
Marek Majkut <marek.majkut@takeda.com>
2022-12-12 15:55:57 +0100

    build template fixing

--- devops/cicd/components/amplify-apps.ts
+++ devops/cicd/components/amplify-apps.ts
@@ -4,39 +4,26 @@ import * as aws from '@cdktf/provider-aws'
 import { getResourceId } from '../utils/param-utils'
 import { REPOSITORY_URL, REPOSITORY_FOLDER_PATH } from '../constants/repo-info'
 
-const bld = {
-  version: 1,
-  applications: [
-    {
-      frontend: {
-        phases: {
-          preBuild: {
-            commands: [
-              'npm ci'
-            ]
-          },
-          build: {
-            commands: [
-              'npm run build'
-            ]
-          }
-        },
-        artifacts: {
-          baseDirectory: 'dist/app',
-          files: [
-            '**/*'
-          ]
-        },
-        cache: {
-          paths: [
-            'node_modules/**/*'
-          ]
-        }
-      },
-      appRoot: REPOSITORY_FOLDER_PATH
-    }
-  ]
-}
+const bld =
+`version: 1
+applications:
+  - frontend:
+      phases:
+        preBuild:
+          commands:
+            - npm ci
+        build:
+          commands:
+            - npm run build
+      artifacts:
+        baseDirectory: dist/app
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+    appRoot: ${REPOSITORY_FOLDER_PATH}
+`
 
 // const amplifyOptions = {
 //   name: getResourceId('my-amplify-frontend'),
@@ -58,7 +45,11 @@ const getAmplifyOptions = (secret: TerraformVariable): aws.amplifyApp.AmplifyApp
     name: getResourceId('my-amplify-frontend'),
     repository: REPOSITORY_URL,
     accessToken: secret.value,
-    buildSpec: JSON.stringify(bld)
+    environmentVariables: {
+      AMPLIFY_DIFF_DEPLOY: 'true',
+      AMPLIFY_MONOREPO_APP_ROOT: 'frontend'
+    },
+    buildSpec: bld
   }
 }
 
--- devops/cicd/constants/repo-info.ts
+++ devops/cicd/constants/repo-info.ts
@@ -1,2 +1,2 @@
-export const REPOSITORY_URL = 'https://github.com/majkutma/proj-demo'
+export const REPOSITORY_URL = 'https://github.com/majkutma/proj-tf'
 export const REPOSITORY_FOLDER_PATH = 'frontend'
